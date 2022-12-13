b8e59f27a42a8dec9ea2ca75b6c68f9245a13f2f
Троицкий Роман Юрьевич <troitsk@komus.net>
2022-12-13 23:35:28 +0300

    feat: body class

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,9 @@ export default defineNuxtConfig({
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
+      bodyAttrs: {
+        class: 'custom-scrollbar',
+      },
     },
   },
 
